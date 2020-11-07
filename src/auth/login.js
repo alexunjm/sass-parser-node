c42bd2e6eff513d07209f2d9dc1e856b50583e1b
Alexander Jaramillo <alexunjm@gmail.com>
2020-11-07 13:04:38 -0500

    comentario
    validar si se puede implementar de una vez ts

--- src/auth/login.js
+++ src/auth/login.js
@@ -1,3 +1,3 @@
 const login = (user, password) => {
-    console.log(`making login for: ${user.name} y este es el password: ${password}`)
+    console.log(`making login for: ${user.name} y este es el password: ${password}`) // si usuario es un objeto, sería bueno pensar en implementar ts
 }
